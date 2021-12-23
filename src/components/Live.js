7a750b7e2a16d1ee66f100ea52354146195d40c6
Michael Lobman <michael.lobman@gmail.com>
2021-12-22 22:20:35 -0500

    Style video grid

--- src/components/Live.js
+++ src/components/Live.js
@@ -3,25 +3,40 @@ import React from "react"
 function Live(){
     return(
         <div>
-            <h2 className="page-title">Dylan Live</h2>
             <div className="video-container">
                 <iframe 
                     className="video"
                     src="https://www.youtube.com/embed/HRrlFYg2QkI" 
                     title="YouTube video player" 
-                    frameborder="0" 
+                    frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen
+                    allowFullScreen
                 >
                 </iframe>
                 <iframe 
                     className="video"
                     src="https://www.youtube.com/embed/YwSZvHqf9qM" 
                     title="YouTube video player" 
-                    frameborder="0" 
+                    frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen
+                    allowFullScreen
                 >
+                </iframe>
+                <iframe 
+                    className="video"
+                    src="https://www.youtube.com/embed/OeP4FFr88SQ" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>
+                </iframe>
+                <iframe 
+                    className="video" 
+                    src="https://www.youtube.com/embed/ujgqOgMIwfA" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>
                 </iframe>
             </div>
         </div>
--- src/components/Moments.js
+++ src/components/Moments.js
@@ -1,8 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Moments(){
+
+    const [name, setName] = useState("");
+    const [moment, setMoment] = useState("");
+
+    console.log(name)
+
+
+    
     return (
-        <h4>Add your favorite Dylan moment here</h4>
+        <div>
+            <h4>Add your favorite Dylan moment here</h4>
+            <form id="add-moment">
+                <label>
+                    Name:
+                    <input 
+                        onChange={e => setName(e.target.value)} 
+                        type="text" id="name" 
+                        name="name" 
+                        value={name}
+                    />
+                </label>
+                <label>
+                    Moment:
+                    <textarea 
+                        onChange={e => setMoment(e.target.value)}
+                        rows="3" 
+                        columns="50" 
+                        id="moment" 
+                        name="moment" 
+                        value={moment}
+                    />
+                </label>
+                <button type="submit">Add Moment</button>
+            </form>
+        </div>
+        
     )
 }
 
