e3830ca2888b8cf89dadaa26826ca81fbfb25254
Michael Lobman <michael.lobman@gmail.com>
2022-01-03 21:33:16 -0500

    Add 2 live videos

--- src/components/Live.js
+++ src/components/Live.js
@@ -6,7 +6,7 @@ function Live(){
             <div className="video-container">
                 <iframe 
                     className="video"
-                    src="https://www.youtube.com/embed/HRrlFYg2QkI" 
+                    src="https://www.youtube.com/embed/YwSZvHqf9qM" 
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
@@ -15,12 +15,20 @@ function Live(){
                 </iframe>
                 <iframe 
                     className="video"
-                    src="https://www.youtube.com/embed/YwSZvHqf9qM" 
+                    src="https://www.youtube.com/embed/HRrlFYg2QkI" 
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                     allowFullScreen
                 >
+                </iframe>
+                <iframe 
+                    className="video" 
+                    src="https://www.youtube.com/embed/ujgqOgMIwfA" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>
                 </iframe>
                 <iframe 
                     className="video"
@@ -31,13 +39,22 @@ function Live(){
                     allowFullScreen>
                 </iframe>
                 <iframe 
-                    className="video" 
-                    src="https://www.youtube.com/embed/ujgqOgMIwfA" 
+                    className="video"
+                    src="https://www.youtube.com/embed/a6Kv0vF41Bc" 
                     title="YouTube video player" 
-                    frameBorder="0" 
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowFullScreen>
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
                 </iframe>
+                <iframe 
+                    className="video" 
+                    src="https://www.youtube.com/embed/7sqAhF6i9H4?start=169" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+                </iframe>
+
             </div>
         </div>
     )
