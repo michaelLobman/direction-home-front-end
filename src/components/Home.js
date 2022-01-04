2808b20a968d69192da04ba72fdd0f8bb4053eb5
Michael Lobman <michael.lobman@gmail.com>
2022-01-03 21:26:36 -0500

    Update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LyricWindow from "./LyricWindow";
 
 function Home() {
+
     return (
         <div>
             <h2 className="page-title">A Shelter from the Storm for Dylan Fans</h2>
             <LyricWindow />
-            <img id="home-img" src="https://media.newyorker.com/photos/5cfd180233dd5c419b77234d/master/pass/Remnick-Netflix.jpg"/>
+            <img 
+                className="album-covers" 
+                alt="Album Cover Art" 
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/5701835_170.jpg" 
+            />
+            <img 
+                className="album-covers" 
+                alt="Album Cover Art" 
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/5701847_147.jpg" 
+            />
+             <img 
+                className="album-covers" 
+                alt="Album Cover Art" 
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/5701850_126.jpg" 
+            />
+            <img
+                className="album-covers"
+                alt="Album Cover Art"
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/5701853_181.jpg"
+            />
+            <img 
+                className="album-covers"
+                alt="Album Cover Art"
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/4469449_278.jpg"
+            />
+            <img
+                className="album-covers"
+                alt="Album Cover Art"
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/3992195_198.jpg"
+            />
+            <img
+                className="album-covers"
+                alt="Album Cover Art"
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/3992196_131.jpg"
+            />
+            <img
+                className="album-covers"
+                alt="Album Cover Art"
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/3992197_165.jpg"
+            />
+            <img 
+                className="album-covers"
+                alt="Album Cover Art"
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2011/10/3992270_169.jpg"
+            />
+            <img
+                className="album-covers"
+                alt="Album Cover Art"
+                src="https://cdn.smehost.net/bobdylancom-uscolumbiaprod/wp-content/uploads/2012/12/tempest-300x300.jpg"
+            />
         </div>
     )
 }
